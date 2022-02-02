96f17df7b6feed8cfbc8950fa7eadb40e5ec1551
willyc <willcowendrums@gmail.com>
2022-02-02 17:12:20 +0000

    all requirements passing - pre refactor

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,25 +1,73 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
-  it("new shop instance returns empty array.", function() {
+var { Shop, Item } = require("../src/gilded_rose.js");
+describe("Gilded Rose", function () {
+  it("new shop instance returns empty array.", function () {
     const gildedRose = new Shop();
-    expect(gildedRose.updateQuality(1)).toEqual([]);
+    expect(gildedRose.updateQuality()).toEqual([]);
   });
-  it("can check value of item in itemarray.", function() {
-    const gildedRose = new Shop([ new Item("example item", 10, 9) ]);
-    const items = gildedRose.updateQuality(1);
+  it("can check value of item in itemarray.", function () {
+    const gildedRose = new Shop([new Item("example item", 10, 9)]);
+    const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("example item");
   });
-  it("item sellIn & item quality decreases by one after one day.", function() {
-    const gildedRose = new Shop([ new Item("example item", 10, 9) ]);
-    const items = gildedRose.updateQuality(1);
+  it("item sellIn & item quality decreases by one after one day.", function () {
+    const gildedRose = new Shop([new Item("example item", 10, 9)]);
+    const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toEqual(9);
     expect(items[0].quality).toEqual(8);
   });
-  it("item sellIn & item quality decreases by 5 after 5 days.", function() {
-    const gildedRose = new Shop([ new Item("example item", 10, 9) ]);
-    const items = gildedRose.updateQuality(5);
+  it("item sellIn & item quality decreases by 5 after 5 days.", function () {
+    const gildedRose = new Shop([new Item("example item", 10, 9)]);
+    for (let i = 0; i < 4; i++) {
+      gildedRose.updateQuality();
+    }
+    const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toEqual(5);
     expect(items[0].quality).toEqual(4);
   });
-
+  it("Once sellIn date passed, quality decreases twice as fast.", function () {
+    const gildedRose = new Shop([new Item("thing", -5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(8);
+  });
+  it("The Quality of an item is never negative.", function () {
+    const gildedRose = new Shop([new Item("Another thing", -1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
+  it("Aged Brie actually increases in Quality the older it gets", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(2);
+  });
+  it("The Quality of an item is never more than 50", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(50);
+  });
+  it("Sulfuras, being a legendary item, never has to be sold or decreases in Quality", function () {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(0);
+    expect(items[0].quality).toEqual(80);
+  });
+  it("Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert", function () {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(22);
+    expect(items[1].quality).toEqual(23);
+    expect(items[2].quality).toEqual(0);
+  });
+  it("Conjured items degrade in Quality twice as fast as normal items", function () {
+    const gildedRose = new Shop([
+      new Item("Conjured Water", 10, 10),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(8);
+  });
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,7 +1,7 @@
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
-    this.sellIn = sellIn; //All items have a SellIn value which denotes the number of days we have to sell the item
+    this.sellIn = sellIn; //All items have a SellIn value which denotes the number of 1 we have to sell the item
     this.quality = quality; //All items have a Quality value which denotes how valuable the item is
   }
 
@@ -12,40 +12,40 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  updateQuality(days) {
+  updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - days;
+            this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + days;
+          this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + days;
+                this.items[i].quality = this.items[i].quality + 1;
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + days;
+                this.items[i].quality = this.items[i].quality + 1;
               }
             }
           }
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - days;
+        this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
+      if (this.items[i].sellIn < 0 || this.items[i].name == 'Conjured Water') {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - days;
+                this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
@@ -53,7 +53,7 @@ class Shop {
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + days;
+            this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
